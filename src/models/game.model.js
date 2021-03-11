8e1e36b1bda200ebf13d5a6cbe3977aee5c3174b
PascaleTran <ptran01.pro@gmail.com>
2021-03-11 20:54:12 +0100

    changemt type et valeur defaut gameSchema

--- src/models/game.model.js
+++ src/models/game.model.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose');
 
 const playerSchema = new mongoose.Schema({
-    nom: String,
-    prenom: String
+  nom: String,
+  prenom: String
 });
 
 const teamSchema = new mongoose.Schema({
-    score: Number,
-    login: String,
-    passe: String,
-    joueurs: [playerSchema]
+  score: Number,
+  login: String,
+  passe: String,
+  joueurs: [playerSchema]
 });
 
 const gameSchema = new mongoose.Schema({
-  dateDebut: String,
+  dateDebut: { type: Date, default: Date.now },
   heureDebut: String,
   heureFin: String,
   equipes: [teamSchema]
