7170424d38eece2293eb86741573f050ca5635e3
PascaleTran <ptran01.pro@gmail.com>
2021-03-11 21:08:36 +0100

    ajout headers pour eviter  erreurs CORS

--- app.js
+++ app.js
@@ -4,15 +4,23 @@ const app = express();
 const port = 3001;
 const Game = require('./src/models/game.model')
 const dotenv = require('dotenv')
-dotenv.config({ path : `${__dirname}/src/config/.env`} )
+dotenv.config({ path: `${__dirname}/src/config/.env` })
 app.use(express.json())
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_TABLE}`, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log('Connexion à MongoDB réussie !')).catch(() => console.log('Connexion à MongoDB échouée !'));
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_TABLE}`, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log('Connexion à MongoDB réussie !')).catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const gamesRouter = require("./src/routes/games")
 const teamsRouter = require("./src/routes/teams")
 const playersRouter = require("./src/routes/players")
 const authRouter = require('./src/routes/auth')
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
 
 app.use('/auth', authRouter);
 app.use('/api/games', gamesRouter);
@@ -20,13 +28,13 @@ app.use('/api/teams', teamsRouter);
 app.use('/api/players', playersRouter);
 
 
-app.get('/', async (req, res)=> {
+app.get('/', async (req, res) => {
     const games = await Game.find({})
-    res.status(200).json({data: games})
+    res.status(200).json({ data: games })
 })
 
 
-app.post('/', (req, res)=> {
+app.post('/', (req, res) => {
     const game = new Game(req.body);
 
     game.save();
@@ -34,7 +42,7 @@ app.post('/', (req, res)=> {
 })
 
 // ERREUR - NOT FOUND
-app.get('*', (req, res)=>{
+app.get('*', (req, res) => {
     res.status(404).send("Personne n'est présent das l'antre de la bête.");
 });
 
