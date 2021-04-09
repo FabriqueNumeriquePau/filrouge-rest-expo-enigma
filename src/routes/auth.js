1e79f7728d28da1499bc090a37990367232492bc
PascaleTran <ptran01.pro@gmail.com>
2021-04-09 14:50:42 +0200

    auth ok

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -12,37 +12,43 @@ const User = require('../models/userModel')
 /*******************************************/
 /*** Routage du système d'authentification */
 router.post('/', (req, res) => {
-    if(!req.body.login || !req.body.password){
-        return res.status(400).json({ message: 'Login ou mot de passe manquant'})
+
+    if (!req.body.login || !req.body.password) {
+        return res.status(400).json({ message: "login ou mot de passe manquant" })
     }
-
+    console.log(req.body);
     // Vérification si le compte existe bien en base
-    User.findOne({ where: { login: req.body.login }, raw: true})
+    User.find({ login: req.body.login })
         .then(user => {
+            console.log('user:', user);
             // Vérification si utilisateur existe
-            if(user === null){
-                return res.status(401).json({ message: 'Compte inexistant'})
-            }            
+            if (user === null) {
+                return res.status(401).json({ message: 'Compte inexistant' })
+            }
+            // //je vérifie si il est admin
+            // if (user[0].admin =='true'){
 
+            // }
             // Je vérifie si le mot de passe est bon
-            bcrypt.compare(req.body.password, user.password)
+            bcrypt.compare(req.body.password, user[0].password)
                 .then(test => {
                     // Si différent donc test=false donc mauvais mot de passe
-                    if(!test){
-                        return res.status(401).json({ message: 'bad password !'})
+                    if (!test) {
+                        return res.status(401).json({ message: 'bad password !' })
                     }
-
+                    console.log('login', process.env.JWT_SECRET)
                     // Les 2 hash correspondent, création du token
                     const token = jwt.sign({
-                        id: user._id,
-                        login: user.login,
+                        id: user[0]._id,
+                        login: user[0].login,
                     }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_DURING })
-        
+                    console.log('token', token)
                     return res.json({ access_token: token })
                 })
                 .catch(err => res.status(401).json({ message: 'Password test error' }))
-            
+
         })
+
         .catch(err => res.json({ message: 'Database error', error: err }))
 });
 
