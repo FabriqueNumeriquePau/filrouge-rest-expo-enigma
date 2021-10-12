9dc31546e20787709d1d13b14c24be7ea2ecf0e4
PascaleTran <ptran01.pro@gmail.com>
2021-10-12 16:25:51 +0200

    verif admin connexion

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -30,22 +30,49 @@ router.post('/', (req, res) => {
 
             // }
             // Je vérifie si le mot de passe est bon
-            bcrypt.compare(req.body.password, user.password)
-                .then(test => {
-                    // Si différent donc test=false donc mauvais mot de passe
-                    if (!test) {
-                        return res.status(401).json({ message: 'bad password !' })
+            // bcrypt.compare(req.body.password, user.password)
+            //     .then(test => {
+            //         // Si différent donc test=false donc mauvais mot de passe
+            //         console.log('test:', test);
+            //         if (!test) {
+            //             return res.status(401).json({ message: 'bad password !' })
+            //         }
+
+            //         console.log('login', process.env.JWT_SECRET)
+            //         // Les 2 hash correspondent, création du token
+            //         const token = jwt.sign({
+            //         id: user._id,
+            //         login: user.login,
+            //     }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_DURING })
+            //     console.log('token', token)
+            //     return res.json({ access_token: token })
+            // })
+            // .catch(err => res.status(401).json({ message: 'Password test error' }))
+            bcrypt.compare(req.body.password, user.password, (err, result) => {
+                console.log('req.body.password', req.body.password);
+                console.log('user.password', user.password);
+                console.log('result', result);
+                if (err) {
+                    res.json({ message: "Impossible de se connecter", err })
+                } else {
+
+                    if (result === true & user.admin === true) {
+                        //Les 2 hash correspondent, et c'est bien un admin alors création du token
+                        const token = jwt.sign({
+                            id: user._id,
+                            login: user.login,
+                        }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_DURING })
+                        console.log('token', token)
+
+
+                        res.json({ success: "Admin connecté", token })
+                    } else {
+                        res.json({
+                            message: "L'identifiant et le mot de passe ne correspondent pas ou vous n'êtes pas administrateur "
+                        })
                     }
-                    console.log('login', process.env.JWT_SECRET)
-                    // Les 2 hash correspondent, création du token
-                    const token = jwt.sign({
-                        id: user._id,
-                        login: user.login,
-                    }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_DURING })
-                    console.log('token', token)
-                    return res.json({ access_token: token })
-                })
-                .catch(err => res.status(401).json({ message: 'Password test error' }))
+                }
+            })
 
         })
 
